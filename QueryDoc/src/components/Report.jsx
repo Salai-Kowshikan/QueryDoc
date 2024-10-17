7353b8b2adcb30627627ba1086cdd0c0e2ac76f9
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-10-17 16:52:25 +0530

    temp push

--- QueryDoc/src/components/Report.jsx
+++ QueryDoc/src/components/Report.jsx
@@ -7,11 +7,21 @@ function Report() {
   const navigate = useNavigate();
   return (
     <>
-      <div>Report</div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nunc nec
-        nisl ultricies Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Sed ac nunc nec nisl ultricies
+      <div className="text-3xl">Summary</div>
+      <p className="font-sans text-lg pt-5">
+        This dataset captures performance metrics for various database
+        operations (Read, Create, Update, Delete) on different databases
+        (PostgreSQL, MongoDB, Sharded MongoDB, and Cassandra). Each operation
+        was conducted on 10,000 records (except some Delete and Update
+        operations on 1 record). Cassandra shows higher times for most
+        operations compared to others, especially on Create and Delete
+        operations. PostgreSQL generally has lower times for both Read and
+        Update operations, while MongoDB has varied times, with higher times in
+        Update and Delete operations. Sharded MongoDB, although faster than
+        Cassandra, is generally slower than PostgreSQL and MongoDB for these
+        specific tasks. This data allows for evaluating each database's relative
+        performance under identical workload conditions Visualize the execution
+        time of the 4 dbs here....
       </p>
       <div className="flex w-full justify-center space-x-4">
         <Button
@@ -21,7 +31,10 @@ function Report() {
           CRUD Chart
         </Button>
 
-        <Button onClick={() => navigate("/overall")} className="bg-black text-white border-2 border-white hover:bg-white hover:text-black font-bold py-2 px-4 rounded flex items-center transition duration-300">
+        <Button
+          onClick={() => navigate("/overall")}
+          className="bg-black text-white border-2 border-white hover:bg-white hover:text-black font-bold py-2 px-4 rounded flex items-center transition duration-300"
+        >
           Overall Chart
         </Button>
       </div>
--- QueryDoc/src/components/data.jsx
+++ QueryDoc/src/components/data.jsx
@@ -16,126 +16,126 @@ export let data = [
     {
         "operation_type": "Read",
         "no_of_records": 10000,
-        "time": 127,
+        "time": 2417,
         "db": "sharded_mongo",
         "qid": 1
     },
     {
         "operation_type": "Read",
         "no_of_records": 10000,
-        "time": 190,
+        "time": 3657,
         "db": "cassandra",
         "qid": 1
     },
     {
         "operation_type": "Read",
         "no_of_records": 10000,
-        "time": 204,
+        "time": 812,
         "db": "mongo",
         "qid": 2
     },
     {
         "operation_type": "Read",
         "no_of_records": 10000,
-        "time": 105,
+        "time": 581,
         "db": "postgres",
         "qid": 2
     },
     {
         "operation_type": "Read",
         "no_of_records": 10000,
-        "time": 140,
+        "time":2132 ,
         "db": "sharded_mongo",
         "qid": 2
     },
     {
         "operation_type": "Read",
         "no_of_records": 10000,
-        "time": 230,
+        "time": 2974,
         "db": "cassandra",
         "qid": 2
     },
     {
         "operation_type": "Create",
-        "no_of_records": 1,
-        "time": 24,
+        "no_of_records": 10000,
+        "time": 523,
         "db": "mongo",
         "qid": 3
     },
     {
         "operation_type": "Create",
-        "no_of_records": 1,
-        "time": 46,
+        "no_of_records": 10000,
+        "time": 1421,
         "db": "postgres",
         "qid": 3
     },
     {
         "operation_type": "Create",
-        "no_of_records": 1,
-        "time": 38,
+        "no_of_records": 10000,
+        "time": 779,
         "db": "sharded_mongo",
         "qid": 3
     },
     {
         "operation_type": "Create",
-        "no_of_records": 1,
-        "time": 52,
+        "no_of_records": 10000,
+        "time": 6210,
         "db": "cassandra",
         "qid": 3
     },
     {
         "operation_type": "Update",
-        "no_of_records": 1,
-        "time": 25,
+        "no_of_records": 10000,
+        "time": 529,
         "db": "postgres",
         "qid": 4
     },
     {
         "operation_type": "Update",
-        "no_of_records": 1,
-        "time": 20,
+        "no_of_records": 10000,
+        "time": 4596,
         "db": "mongo",
         "qid": 4
     },
     {
         "operation_type": "Update",
-        "no_of_records": 1,
-        "time": 27,
+        "no_of_records": 10000,
+        "time": 3296,
         "db": "sharded_mongo",
         "qid": 4
     },
     {
         "operation_type": "Update",
-        "no_of_records": 1,
-        "time": 30,
+        "no_of_records": 10000,
+        "time":4256 ,
         "db": "cassandra",
         "qid": 4
     },
     {
         "operation_type": "Delete",
-        "no_of_records": 1,
-        "time": 21,
+        "no_of_records": 10000,
+        "time": 3480,
         "db": "mongo",
         "qid": 5
     },
     {
         "operation_type": "Delete",
-        "no_of_records": 1,
-        "time": 222,
+        "no_of_records": 10000,
+        "time": 295,
         "db": "postgres",
         "qid": 5
     },
     {
         "operation_type": "Delete",
-        "no_of_records": 1,
-        "time": 40,
+        "no_of_records": 10000,
+        "time": 1280,
         "db": "sharded_mongo",
         "qid": 5
     },
     {
         "operation_type": "Delete",
-        "no_of_records": 1,
-        "time": 70,
+        "no_of_records": 10000,
+        "time":3890 ,
         "db": "cassandra",
         "qid": 5
     }
